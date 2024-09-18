8093534c9d619d21db98015ec4fa72454ce1b61b
Brayan Casallas <142183781+CowardOp@users.noreply.github.com>
2024-09-18 13:21:08 -0500

    Create cowardop.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/cowardop.js
@@ -0,0 +1,42 @@
+//EJERCICIO:
+
+//Este es un comentario con la URL del la web oficial de JavaScript:  * https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//Comentario en una linea
+
+
+/*este tipo de cometario
+se asigna a diferentes lineas
+*/
+
+//Variable y constante
+
+let variable = true
+const constante = boolean
+console.log(variable)
+
+//datos primitivos
+
+let string = "JavaScript"
+let numero = 25
+let numeroGrande = 351654321685416365498n
+let flotante = 12.52
+let boolean = [true,false]
+let objeto = null
+let indefinido;
+let simbolo = Symbol ("simbolo1")
+
+//imprime hola javascript
+
+console.log(`Hola, ${string}`)
+
+
+
+                    
+
+                      
+
+
+
+
+
